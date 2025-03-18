23d59c8d39dc9c1ecad075958afb994b7a4f22d5
MagnusSoderholm <magnus.soderholm@hotmail.com>
2025-03-18 11:27:29 +0100

    Update

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using WEbbLIE.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+
 // Add MudBlazor services
 builder.Services.AddMudServices();
 
@@ -19,6 +21,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 
